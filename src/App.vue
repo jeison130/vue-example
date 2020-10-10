3ac1f47b2264c53e05b7a14385491f21be2aff6f
jeison130 <jeison130@gmail.com>
2020-10-09 19:37:39 -0500

    Cambiando texto about

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Inicio</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">Acerca de</router-link>
   </div>
   <router-view/>
 </template>
